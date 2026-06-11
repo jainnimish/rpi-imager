008ef0440888045ef6effaecd409909cc264dac7
Nimish Jain <njain15@protonmail.com>
2026-06-11 15:14:06 -0400

    Update Qt build scripts for FreeBSD

--- qt/qt-build-common.sh
+++ qt/qt-build-common.sh
@@ -679,13 +679,13 @@ install_linux_basic_deps() {
     fi
 }
 
-install_freebsd_deps() {
+install_basic_freebsd_deps() {
     if [ "$SKIP_DEPENDENCIES" -eq 0 ]; then
         echo "Installing basic build dependencies..."
 
         sudo pkg update
-        sudo pkg install cmake ninja pkgconf perl5 python
-        sudo pkg install icu pcre2 freetype2 fontconfig openssl
+        sudo pkg install -y cmake ninja pkgconf perl5 python
+        sudo pkg install -y icu pcre2 freetype2 fontconfig openssl
 
         echo "FreeBSD dependencies installed via pkg"
     fi
